99590de0ad45f7e0b8520b519a989f23998a0fd3
chiweenie6 <113393706+Chiweenie6@users.noreply.github.com>
2023-04-17 23:05:50 -0700

    updated server.js

--- server/server.js
+++ server/server.js
@@ -25,11 +25,11 @@ app.use(express.json());
 // if we're in production, serve client/build as static assets
 app.use(express.static(path.join(__dirname, "..", "build")));
 
-app.get("/", (req, res) => {
+app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "..", "build"));
 });
 
-app.get('*', function(req,res){
+app.get('*', function (req,res) {
   res.sendFile(path.join(__dirname, '..', 'build/index.html'));
 });
 
