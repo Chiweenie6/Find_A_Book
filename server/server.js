9f57d1a9da780a308c6da0dfe5522abee78f6a5b
chiweenie6 <113393706+Chiweenie6@users.noreply.github.com>
2023-04-17 23:00:18 -0700

    updated server.js

--- server/server.js
+++ server/server.js
@@ -23,12 +23,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-}
+app.use(express.static(path.join(__dirname, "..", "build")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "..", "build"));
+});
+
+app.get('*', function(req,res){
+  res.sendFile(path.join(__dirname, '..', 'build/index.html'));
 });
 
 // New instance of an Apollo server created with the GraphQL schema
