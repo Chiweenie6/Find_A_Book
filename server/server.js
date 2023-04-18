1d1b31e98d212c3207c738dfb9cf68ccb0161012
chiweenie6 <113393706+Chiweenie6@users.noreply.github.com>
2023-04-17 17:13:27 -0700

    updated server.js

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const { authMiddleware } = require("./utils/auth");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.set("port", PORT)
+app.set("port", PORT);
 
 // Using Apollo Servernpm run build
 const server = new ApolloServer({
